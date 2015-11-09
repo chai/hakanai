5cda30afcce607d9f2c12df59299e100be33532f
Chai <chai.nge@aus.fujixerox.com>
2015-11-09 10:13:14 +0900

    initial

---
+++ hakanai/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hakanai.Startup))]
+namespace hakanai
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
